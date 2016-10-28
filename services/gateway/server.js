2f4e21be98f4d4a1b9311791742f51d44a8e6509
bbartolome <bbartolome@users.noreply.github.com>
2016-10-27 22:22:33 -0400

    feat: queue events with context info
    Add context info to events - i.e. user and view

--- services/gateway/server.js
+++ services/gateway/server.js
@@ -9,12 +9,43 @@ nconf
 var express = require('express');
 
 // TODO: add nconf for other JWT options
-var api = require('eq8')({jwt: {secret: nconf.get('secret')}});
+var api = require('eq8')({
+	jwt: {secret: nconf.get('secret')},
+	handlers: {
+		async: function asyncHandler(ws, next) {
+			var self = this;
+
+			ws.on('message', function messageHandler(message) {
+				var e;
+				try {
+					e = JSON.parse(message);
+					e.user = ws.user;
+					e.view = ws.route;
+				} catch (ex) {
+					self.logger.error('WebSocket message was not in JSON', message);
+					ws.send(JSON.stringify({error: {status: '400', title: 'bad-request'}}));
+				}
+
+				if(e) {
+					self.trigger(e);
+				}
+			})
+		}
+	}
+});
+
+var seneca = require('seneca')();
+seneca.use(require('seneca-beanstalk-transport'));
+var client = seneca.client({
+	type: 'beanstalk',
+	host: nconf.get('HOST_QUEUE')
+});
+api.on('trigger', seneca.act.bind(seneca));
+
 var app = express();
 
 app.use(express.static(nconf.get('public')));
 
 app.use('/api', api.syncware);
 
-app.listen(nconf.get('port'));
-
+api.emit('listening', app.listen(nconf.get('port')));
