fb6ce2cf8cd5ef0bf63b6079631f4931b0203096
bbartolome <bbartolome@users.noreply.github.com>
2016-10-27 23:11:58 -0400

    feat: listen for queued events on `search`

---
+++ services/processor/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var nconf = require('nconf');
+nconf
+	.argv()
+	.env()
+	.file({file: './defaults.json'});
+
+var api = require('eq8-api')();
+
+var seneca = require('seneca')();
+seneca.use(require('seneca-beanstalk-transport'));
+var listen = seneca.listen({
+	type: 'beanstalk',
+	host: nconf.get('HOST_QUEUE')
+});
+api.on('search', seneca.add.bind(seneca));
