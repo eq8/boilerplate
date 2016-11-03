6ee021648d95f2e75d9b24f93e8207f3057cb8a3
bbartolome <bbartolome@users.noreply.github.com>
2016-11-02 23:27:07 -0400

    feat(processor): add transaction context when calling the `state` method

--- services/processor/index.js
+++ services/processor/index.js
@@ -6,7 +6,35 @@ nconf
 	.env()
 	.file({file: './defaults.json'});
 
-var api = require('eq8-api')();
+var knex = require('knex')(
+	client: 'mysql',
+	connection: {
+		host : nconf.get('HOST_DB'),
+		database : nconf.get('db')
+	}
+);
+
+var api = require('eq8-api')({
+	registrars: {
+		actions: function(actions, callback) {
+			Rx.Observable.from(actions)
+				.subscribe(
+					function(action) {
+						api.search(action, function(args, done) {
+							knex.transaction(function(trx) {
+								api.state({
+									trx: trx
+								}, args, done);
+							});
+						})
+					},
+					callback,
+					callback
+				);
+		}
+	}
+});
+var Rx = require('rx');
 
 var seneca = require('seneca')();
 seneca.use(require('seneca-beanstalk-transport'));
@@ -15,3 +43,6 @@ var listen = seneca.listen({
 	host: nconf.get('HOST_QUEUE')
 });
 api.on('search', listen.add.bind(listen));
+
+api.addRegistrar({
+})
