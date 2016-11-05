537e7f08d67e9734fc96bdfed502a6126136f1cb
bbartolome <bbartolome@users.noreply.github.com>
2016-11-04 20:20:38 -0400

    fix(processor): redo transaction context passing and fix defaults

--- services/processor/index.js
+++ services/processor/index.js
@@ -14,35 +14,50 @@ var knex = require('knex')(
 	}
 );
 
-var api = require('eq8-api')({
-	registrars: {
-		actions: function(actions, callback) {
+var api = require('eq8-api')();
+var Rx = require('rx');
+
+var seneca = require('seneca')();
+seneca.use(require('seneca-beanstalk-transport'));
+
+var listen = seneca.listen({
+	type: 'beanstalk',
+	host: nconf.get('HOST_QUEUE')
+});
+
+api.on('search', listen.add.bind(listen));
+
+api.on('state', function(ctxt, msg, done) {
+	ctxt.trx
+		.insert({
+			desc: ctxt.name,
+			msg: JSON.stringify(msg);
+		})
+		.into(nconf.get('logtable'))
+		.asCallback(done);
+});
+
+api.addRegistrar({
+	actions: function(actions, callback, prior) {
+		prior(actions, function() {
 			Rx.Observable.from(actions)
 				.subscribe(
 					function(action) {
-						api.search(action, function(args, done) {
+						api.search(action.pattern, function(msg, done) {
 							knex.transaction(function(trx) {
-								api.state({
-									trx: trx
-								}, args, done);
+								api.state({name: action.name, trx: trx}, msg, function(err) {
+									if(err) {
+										return trx.rollback();
+									}
+
+									return trx.commit();
+								});
 							});
 						})
 					},
 					callback,
 					callback
 				);
-		}
+		});
 	}
 });
-var Rx = require('rx');
-
-var seneca = require('seneca')();
-seneca.use(require('seneca-beanstalk-transport'));
-var listen = seneca.listen({
-	type: 'beanstalk',
-	host: nconf.get('HOST_QUEUE')
-});
-api.on('search', listen.add.bind(listen));
-
-api.addRegistrar({
-})
