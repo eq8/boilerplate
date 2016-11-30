76fa4a8f3d994c635a1d7f76a5c4fb8dba6cb4a2
bbartolome <bbartolome@users.noreply.github.com>
2016-11-30 00:46:08 -0500

    feat(indexer): create pubsub listener

--- services/indexer/index.js
+++ services/indexer/index.js
@@ -6,6 +6,17 @@ nconf
 	.env()
 	.file({file: './defaults.json'});
 
-// var api = require('eq8-api')();
+var api = require('eq8-api')();
 
-// var seneca = require('seneca')();
+var seneca = require('seneca')();
+seneca.use(require('seneca-redis-transport'));
+
+var listen = seneca.listen({
+	type: 'redis',
+	host: nconf.get('PUBSUB_HOST')
+});
+
+api.on('subscribe', function() {
+	this.logger.trace('dispatch:', arguments);
+	listen.add.apply(listen, arguments);
+});
