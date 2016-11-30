e07615002c866d560b3209b704dba9199ddaa0eb
bbartolome <bbartolome@users.noreply.github.com>
2016-11-30 00:30:27 -0500

    feat(notifier): create pubsub client

--- services/notifier/index.js
+++ services/notifier/index.js
@@ -6,6 +6,17 @@ nconf
 	.env()
 	.file({file: './defaults.json'});
 
-// var api = require('eq8-api')();
+var api = require('eq8-api')();
 
-// var seneca = require('seneca')();
+var seneca = require('seneca')();
+seneca.use(require('seneca-redis-transport'));
+
+var client = seneca.client({
+	type: 'redis',
+	host: nconf.get('PUBSUB_HOST')
+});
+
+api.on('dispatch', function() {
+	this.logger.trace('dispatch:', arguments);
+	client.act.apply(client, arguments);
+});
