79434e911d0bfa6628384b2793bcab632fd091af
bbartolome <bbartolome@users.noreply.github.com>
2016-12-15 00:58:16 -0500

    fix(vcs): simplify version control and move all logic into processor

--- services/vcs/index.js
+++ services/vcs/index.js
@@ -19,37 +19,8 @@ var knex = require('knex')({
 // TODO: add defaults for migration config
 knex.migrate.latest();
 
-var api = require('eq8-api')();
 var transport = require('seneca')();
 transport.use(require('seneca-redis-transport'));
-
-var listen = transport.listen({
-	port: nconf.get('listenPort')
-});
-
-api.on('subscribe', function() {
-	this.logger.trace('subscribe:', arguments);
-	listen.add.apply(listen, arguments);
-});
-
-api.subscribe({to: 'vcs'}, function(msg, done) {
-	knex.transaction(function(trx) {
-		var commit = {
-			ns: 'vcs',
-			cmd: 'commit',
-			type: 'transaction',
-			body: msg.body
-		};
-		api.state({trx: trx}, commit, function(err) {
-			if(err) {
-				trx.rollback();
-				// TODO: queue an action to notify user that the recent action failed
-			} else {
-				trx.commit();
-			}
-		});
-	}).asCallback(done);
-});
 
 var client = transport.client({
 	type: 'redis',
@@ -57,90 +28,36 @@ var client = transport.client({
 	port: nconf.get('pubsubPort')
 });
 
-api.on('dispatch', function() {
-	this.logger.trace('dispatch:', arguments);
-	client.act.apply(client, arguments);
+var listen = transport.listen({
+	port: nconf.get('listenPort')
 });
 
 var Rx = require('rx');
 var _ = require('lodash');
 
-api.register({
-	actions: [
-		{
-			name: 'commit',
-			pattern: {ns: 'vcs', cmd: 'commit', type: 'transaction'},
-			handler: function(ctxt, args, done) {
-				var api = this;
-				var callback = _.once(done);
+listen.add({to: 'vcs'}, function(msg, done) {
+	var callback = _.once(done);
 
-				Rx.Observable
-					.fromArray(Object.keys(args))
-					.concatMap(function(type) {
-						return Rx.Observable
-							.fromArray(Object.keys(args.object[type]))
-							.concatMap(function(id) {
-								return Rx.Observable
-									.fromNodeCallback(api.state, api)(
-										ctxt, {
-											ns: 'vcs', cmd: 'denormalize',
-											type: type,
-											id: id,
-											object: args.object[type][id]
-										}
-									);
-							});
-					})
-					.reduce(function(denormalized, entity) {
-						return _.merge({}, denormalized, entity);
-					}, {})
-					.subscribeOnNext(function(denormalized) {
-						api.dispatch({
-							to: 'notifier',
-							body: denormalized
-						}, callback);
-					})
-					.subscribeOnError(callback);
-			}
-		}
-	]
+	knex.transaction(function(trx) {
+		Rx.Observable
+			.fromArray(msg.items)
+			.concatMap(function(item) {
+				var insert = ctx.trx
+				insert({
+					type: item._type,
+					id: item._id,
+					version: item._version
+				})
+				.into('version');
+
+				return Rx.Observable
+					.fromNodeCallback(insert.asCallback, insert);
+			})
+			.subscribeOnError(callback)
+			.subscribeOnComplete(callback);
+	}).asCallback(function(err) {
+		if(err) return done(err);
+
+		client.act({to: 'all', items: msg.items}, done);
+	});
 });
-
-/*
-TODO:
-create registrar for values
-- it should register some new views (eq8-api) which in turn registers new query types in graphql (eq8)
-- it should register commit actions for the value
-create registrar for entities
-- sample params: namespace, entity, version
-- it should register commit actions for the entity
-create an action to register entities via messages
-register value types
-install seneca-knex-store
-implement queries in seneca-knex-store using registered value types
-*/
-
-/*
-downstream message schema
-{
-	vcs: {
-		entity(uuid???): {
-			id: uuid,
-			version: bigInteger
-			attribute: value
-		}
-	},
-	index: {
-		entity(uuid???): {
-			mapping: {},
-			insertSource: {
-				locale: {
-				}
-			}
-			previousVersions: [
-				{schemaVersion, locale, entity, id}
-			]
-		}
-	}
-}
-*/
