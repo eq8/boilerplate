0f4f702baa0328d85cfe75ae13ec014ae0fbcbd4
bbartolome <bbartolome@users.noreply.github.com>
2016-12-18 22:30:17 -0500

    chore(vcs): move all schema initialization into one migration script

--- services/vcs/migrations/20161202000000_initialize.js
+++ services/vcs/migrations/20161202000000_initialize.js
@@ -1,5 +1,13 @@
 exports.up = function(knex, Promise) {
 	return Promise.all([
+		knex.schema.createTable('version', function(t) {
+			t.string('instance');
+			t.string('type');
+			t.uuid('id');
+			t.bigInteger('version');
+
+			t.unique(['instance', 'type', 'id', 'version']);
+		}),
 		knex.schema.createTable('entity', function(t) {
 			t.string('entity');
 			t.string('attribute');
@@ -12,6 +20,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
 	return Promise.all([
+		knex.schema.dropTable('version'),
 		knex.schema.dropTable('entity')
 	]);
 };
