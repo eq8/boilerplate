f756c35c497488a8f0e6f2962f172e7c5ccc3548
bbartolome <bbartolome@users.noreply.github.com>
2017-01-01 12:12:08 -0500

    fix(logic): add user context info per request

--- services/logic/index.js
+++ services/logic/index.js
@@ -29,6 +29,11 @@ var app = connect();
 var bodyParser = require('body-parser');
 var server = http.createServer(app);
 
+var passport = require('passport');
+var AnonymousStrategy = require('passport-anonymous').Strategy;
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+
 // Create the settings object - see default settings.js file for other options
 var settings = {
 	httpAdminRoot:'/admin',
@@ -39,15 +44,46 @@ var settings = {
 // Initialise the runtime with a server and settings
 RED.init(server, settings);
 
+var passportJwtOptions = {
+	secretOrKey: nconf.get('jwt:secret'),
+	jwtFromRequest: ExtractJwt.fromAuthHeader(),
+	algorithms: nconf.get('jwt:verify:algorithms'),
+	issuer: nconf.get('jwt:verify:issuer'),
+	audience: nconf.get('jwt:verify:audience'),
+	ignoreExpiration: nconf.get('jwt:verify:ignoreExpiration')
+};
+
+passport.use(new JwtStrategy(passportJwtOptions, function(payload, done) {
+	done(null, payload);
+}));
+passport.use(new AnonymousStrategy());
+
+app.use(passport.initialize());
+
+app.use(function initAnonymousUser(req, res, next) {
+	if (!req.user) {
+		req.user = nconf.get('jwt:users:anon');
+	}
+
+	next();
+});
+
 app.use(nconf.get('apiRoot'), bodyParser.json());
 
-// Serve the editor UI from /admin
 app.use(settings.httpAdminRoot, RED.httpAdmin);
 
-app.use(nconf.get('apiRoot') + '/actions', function(req, res) {
-	api.state({}, req.body);
-	res.json({ok: true});
-	res.end();
+app.use(nconf.get('apiRoot') + '/actions', function(req, res, next) {
+	api.state({user: req.user}, req.body);
+	next();
+});
+
+app.use(function(error, req, res, next) {
+	if(error) {
+		res.statusCode = 500;
+		res.end();
+	} else {
+		next();
+	}
 });
 
 server.listen(nconf.get('port'));
