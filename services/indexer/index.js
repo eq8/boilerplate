6993625d54f12c5aeff425e3f0868bd08f35d5ea
bbartolome <bbartolome@users.noreply.github.com>
2017-04-28 22:23:57 -0400

    feat(indexer): draft out the basic logic for indexing objects

--- services/indexer/index.js
+++ services/indexer/index.js
@@ -15,12 +15,40 @@ var listen = transport.listen({
 	pin: 'to:broadcast'
 });
 
-/*
 var Rx = require('rx');
 var _ = require('lodash');
-*/
+var elasticsearch = require('elasticsearch')
+var hostString = nconf.get('indexHost') + ':' + nconf.get('indexPort');
+var client = new elasticsearch.Client({
+	host: hostString
+});
+var indexKey = nconf.get('indexKey');
 
 listen.add({to: 'broadcast'}, function(msg, done) {
-	console.log('msg:', msg);
-	done();
+	Rx.Observable
+		.from(msg.items)
+		.map(item => {
+			return item[indexKey]
+		})
+		.reduce((item, currentValue) => {
+			if(_.has(item, 'schemaVersion')) {
+				switch(item.schemaVersion) {
+					case '0.1':
+					default:
+						currentValue = _.concat(currentValue, item.bulkItems)
+						break;
+				}
+			}
+			return currentValue;
+		}, [])
+		.concatMap(bulkItems => {
+			return Rx.Observable
+				.fromNodeCallback(client.bulk, client)(bulkItems);
+		})
+		.subscribeOnError(err => {
+			done(err);
+		})
+		.subscribeOnCompleted(() => {
+			done();
+		});
 });
