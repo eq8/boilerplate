854e961ddb2cfecefe49bbed88d3175022374d33
bbartolome <bbartolome@users.noreply.github.com>
2017-04-29 15:56:14 -0400

    fix: remove entity table for now - i.e. not yet needed

--- services/vcs/migrations/20161202000000_initialize.js
+++ services/vcs/migrations/20161202000000_initialize.js
@@ -6,20 +6,12 @@ exports.up = function(knex, Promise) {
 			t.bigInteger('version');
 
 			t.unique(['type', 'id', 'version']);
-		}),
-		knex.schema.createTable('entity', function(t) {
-			t.string('entity');
-			t.string('attribute');
-			t.string('type');
-
-			t.primary(['entity', 'attribute']);
 		})
 	]);
 };
 
 exports.down = function(knex, Promise) {
 	return Promise.all([
-		knex.schema.dropTable('version'),
 		knex.schema.dropTable('entity')
 	]);
 };
