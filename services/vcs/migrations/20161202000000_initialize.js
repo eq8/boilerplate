2e18d8bbea79391b369b7d2ab5879cad19374226
bbartolome <bbartolome@users.noreply.github.com>
2017-05-10 15:43:01 -0400

    fix(vcs): add `shard` column in `version` table

--- services/vcs/migrations/20161202000000_initialize.js
+++ services/vcs/migrations/20161202000000_initialize.js
@@ -1,17 +1,18 @@
 exports.up = function(knex, Promise) {
 	return Promise.all([
 		knex.schema.createTable('version', function(t) {
+			t.uuid('shard');
 			t.string('type');
 			t.uuid('id');
 			t.bigInteger('version');
 
-			t.unique(['type', 'id', 'version']);
+			t.unique(['shard', 'type', 'id', 'version']);
 		})
 	]);
 };
 
 exports.down = function(knex, Promise) {
 	return Promise.all([
-		knex.schema.dropTable('entity')
+		knex.schema.dropTable('version')
 	]);
 };
